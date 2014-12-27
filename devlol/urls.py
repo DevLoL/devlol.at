228d3dadb7dca011d9e2ddd009c8fdd5d4dd7eb5
doebi <christoph@doeberl.at>
2014-12-27 22:53:01 +0100

    foobar

--- devlol/urls.py
+++ devlol/urls.py
@@ -1,18 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from devlol.views import Index
-
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'devlol.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', Index.as_view(), name='index'),
+    url(r'^$',                                                                      'devlol.views.home'),
+    url(r'^diary/$',                                                                'diary.views.calendar'),
+    url(r'^diary/event/(?P<event_id>[0-9]+)/$',                                     'diary.views.item'),
+    url(r'^diary/(?P<day>[0-9]{2})-(?P<month>[0-9]{2}-(?P<year>[0-9]{4}))/$',       'diary.views.day'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
-
-
-
--- devlol/views.py
+++ devlol/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import View
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from diary.models import DiaryItem, ImageItem
+import datetime
 
-class Index(View):
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('django 1.7 on Openshift')
+def home(request):
+    diary_items = DiaryItem.objects.filter(date__lte=datetime.date.today()).order_by('-date', '-time');
+    for di in diary_items:
+        image_items = ImageItem.objects.filter(diary_item=di)
+        di.images = image_items
+    events = DiaryItem.objects.filter(date__gt=datetime.date.today()).order_by('date', 'time');
+    return render_to_response("index.html", {"items": diary_items, "events": events})
