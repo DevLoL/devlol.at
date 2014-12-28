77fb968f01fe18c82870284a904bcf366719bb09
doebi <christoph@doeberl.at>
2014-12-28 02:22:00 +0100

    shtuff

--- devlol/urls.py
+++ devlol/urls.py
@@ -2,10 +2,18 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$',                                                                      'devlol.views.home'),
-    url(r'^diary/$',                                                                'diary.views.calendar'),
+    url(r'^$',                                                                      'devlol.views.index'),
+
+    # Static Stuff
+    url(r'^location/$',                                                             'devlol.views.location'),
+    url(r'^projects/$',                                                             'devlol.views.projects'),
+    url(r'^members/$',                                                              'devlol.views.members'),
+
+    # Diary Stuff
+    url(r'^diary/$',                                                                'devlol.views.index'),
     url(r'^diary/event/(?P<event_id>[0-9]+)/$',                                     'diary.views.item'),
     url(r'^diary/(?P<day>[0-9]{2})-(?P<month>[0-9]{2}-(?P<year>[0-9]{4}))/$',       'diary.views.day'),
 
+    # Admin Stuff
     url(r'^admin/', include(admin.site.urls)),
 )
--- devlol/views.py
+++ devlol/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render_to_response
 from diary.models import DiaryItem, ImageItem
 import datetime
 
-def home(request):
+def index(request):
     diary_items = DiaryItem.objects.filter(date__lte=datetime.date.today()).order_by('-date', '-time');
     for di in diary_items:
         image_items = ImageItem.objects.filter(diary_item=di)
         di.images = image_items
     events = DiaryItem.objects.filter(date__gt=datetime.date.today()).order_by('date', 'time');
     return render_to_response("index.html", {"items": diary_items, "events": events})
+
+def location(request):
+    return render_to_response("location.html")
+
+def projects(request):
+    return render_to_response("projects.html")
+
+def members(request):
+    return render_to_response("members.html")
