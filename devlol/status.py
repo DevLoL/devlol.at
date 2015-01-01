cca4495e4683bce390e53c4bc49638135a1a9820
doebi <christoph@doeberl.at>
2015-01-01 17:37:48 +0100

    markdown in diary items

--- devlol/status.py
+++ devlol/status.py
@@ -3,3 +3,9 @@ import urllib
 def query_api(mode='viewstatus'):
     url = "https://devlol.org/status/hackerspaceapi/"
     return urllib.urlopen(url + mode).read()
+
+def get():
+    try:
+        return 'OPEN' in query_api()
+    except:
+        return None
--- devlol/views.py
+++ devlol/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render_to_response
 from diary.models import DiaryItem, ImageItem
+from markdown import markdown
 import datetime
 import status
 
 context = {}
-context['state'] = 'OPEN' in status.query_api()
+context['state'] = status.get()
 context['events'] = DiaryItem.objects.filter(date__gt=datetime.date.today()).order_by('date', 'time');
 
 def index(request):
     diary_items = DiaryItem.objects.filter(date__lte=datetime.date.today()).order_by('-date', '-time');
     for di in diary_items:
+        di.html = markdown(di.content)
+    context['items'] = diary_items
+    """
+    for di in diary_items:
         image_items = ImageItem.objects.filter(diary_item=di)
         di.images = image_items
     context['items'] = diary_items
+    """
     return render_to_response("index.html", context)
 
 def location(request):
