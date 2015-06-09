03848598e373a29181b73ef8976cc08d83f28636
Thomas R. Koll <tomk32@gmail.com>
2015-06-09 09:53:02 +0200

    Display a lot more info on the diary admin list
    Just the name isn't enough information on that list.

--- diary/admin.py
+++ diary/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from diary.models import DiaryItem, EventLocation, ImageItem
 
+class DiaryAdmin(admin.ModelAdmin):
+  list_display = ('title', 'start_date', 'start_time', 'author', 'location')
+
 # Register your models here.
-admin.site.register(DiaryItem)
+admin.site.register(DiaryItem, DiaryAdmin)
 admin.site.register(EventLocation)
 admin.site.register(ImageItem)
